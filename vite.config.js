18676ddd315f95cc07694c1b7c8bbbc99e35c97d
heliogustavo <jacomehelioiq@gmail.comgit config --global user.name heliogustavogit config --global user.email jacomehelioiq@gmail.com>
2024-01-22 14:38:29 -0400

    erro vercel corrigido

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist', // O diretório de saída será "dist"
-  },
   plugins: [react()],
 })
